19e5f32e6261d5759731942a477f80010bf0cd9d
Arseniy <arsenimarchenko@bk.ru>
2025-02-12 19:21:30 +0300

    fix: Added forward ref for Button component

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { FC, HTMLAttributes, ReactNode } from 'react';
+import { FC, forwardRef, HTMLAttributes, ReactNode } from 'react';
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
@@ -11,18 +11,17 @@ const classNames: Record<ButtonProps['variant'], string> = {
     'before:bg-white before:absolute before:opacity-0 hover:before:opacity-20 before:size-full before:left-1/2 before:top-1/2 before:-translate-1/2 before:p-6 before:rounded-md transition-opacity before:transition-opacity active:before:opacity-25',
 };
 
-export const Button: FC<ButtonProps> = ({
-  children,
-  className,
-  variant,
-  ...props
-}) => {
-  return (
-    <button
-      className={clsx('relative', classNames[variant], className)}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
+export const Button: FC<ButtonProps> = forwardRef<
+  HTMLButtonElement,
+  ButtonProps
+>(({ children, className, variant, ...props }, ref) => (
+  <button
+    className={clsx('relative', classNames[variant], className)}
+    {...props}
+    ref={ref}
+  >
+    {children}
+  </button>
+));
+
+Button.displayName = 'Button';
